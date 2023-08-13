ac1f762c79fd0eeeaa56870942fbc8224c2431e8
thaisdsandim <contatothais.sandim@gmail.com>
2023-08-12 21:40:54 -0400

    [UPDATE] finish login and register

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -11,31 +11,82 @@
         <el-col>
           <el-button type="primary" class="login mb-10" @click="showLogin">ENTRAR</el-button>
         </el-col>
-      <el-col><el-button class="register" @click="register">CADASTRAR</el-button></el-col>
+      <el-col><el-button class="register" @click="showRegister">CADASTRAR</el-button></el-col>
     </el-row>
-    <el-dialog v-model="isLoginVisible" title="Escolha a conta para entrar" class="loginDialog">
+
+    <el-dialog v-model="isLoginVisible" title="Escolha a conta para entrar" class="dialog">
       <GoogleLogin :callback="callback"/>
+    </el-dialog>
+
+    <el-dialog v-model="isRegisterVisible" title="Cadastre-se no Geri" class="dialog">
+      Deixe seu contato que retornaremos com informações sobre planos e valores:
+      <el-form @submit.prevent="validateForm" class="mt-20">
+        <el-form-item label="Nome">
+          <el-input type="name" v-model="register.from_name" placeholder="Nome Completo" required/>
+        </el-form-item>
+        <el-form-item label="Email">
+          <el-input type="email" v-model="register.from_email" placeholder="seu@email.com.br" required/>
+        </el-form-item>
+        <el-form-item label="Mensagem">
+          <el-input type="textarea" v-model="register.message" placeholder="Digite sua mensagem aqui..."/>
+        </el-form-item>
+        <el-form-item class="submit-button">
+          <el-button type="primary" native-type="submit">ENVIAR</el-button>
+          <el-button class="register" @click="hideRegister">CANCELAR</el-button>
+        </el-form-item>
+      </el-form>
     </el-dialog>
   </div>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
-import { ElRow, ElCol, ElButton, ElDialog, ElMessage } from 'element-plus';
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '../stores/store';
 import axios from 'axios';
+import emailjs from 'emailjs-com';
 import URL from '../config/apiConfig';
+import {
+  ElRow,
+  ElCol,
+  ElButton,
+  ElDialog,
+  ElMessage,
+  ElForm,
+  ElFormItem,
+  ElInput
+} from 'element-plus';
 
 const router = useRouter();
-
+const authStore = useAuthStore();
 const isLoginVisible = ref(false);
+const isRegisterVisible = ref(false);
+const serviceId = 'service_866m4dz';
+const templateId = 'template_guxh768';
+const userId = '07lhIkLQvaxZGcFuL';
+
+const register = ref({
+  from_name: '',
+  from_email: '',
+  message: ''
+});
+
+const resetRegisterForm = () => {
+  register.value.from_name = '';
+  register.value.from_email = '';
+  register.value.message = '';
+};
 
 const showLogin = () => {
   isLoginVisible.value = true;
 };
 
-const register = () => {
-  router.push('/cadastrar');
+const showRegister = () => {
+  isRegisterVisible.value = true;
+};
+
+const hideRegister = () => {
+  isRegisterVisible.value = false;
 };
 
 const callback = async (response) => {
@@ -47,13 +98,16 @@ const callback = async (response) => {
     const apiResponse = await axios.post(`${URL}/users/sign_in`, data);
 
     if (apiResponse.status === 200) {
-      console.log(apiResponse.data)
+      authStore.setUser(response);
       ElMessage({
         showClose: true,
         message: 'Usuário logado com sucesso!',
         type: 'success',
       });
       isLoginVisible.value = false;
+      setTimeout(() => {
+        router.push(`/dashboard`);
+      }, 1000);
     } else {
       ElMessage({
         showClose: true,
@@ -70,6 +124,46 @@ const callback = async (response) => {
     });
     isLoginVisible.value = false;
   }
+};
+
+const validateForm = () => {
+  const fullName = register.value.from_name.split(' ');
+  if (fullName.length < 2) {
+    ElMessage({
+      showClose: true,
+      message: 'Digite seu nome completo!',
+      type: 'warning',
+    });
+    return;
+  }
+
+  const templateParams = { ...register.value };
+
+  submitForm(templateParams);
+  resetRegisterForm();
+};
+
+const submitForm = (templateParams) => {
+
+  emailjs.send(serviceId, templateId, templateParams, userId)
+    .then(response => {
+      console.log(response);
+      ElMessage({
+        showClose: true,
+        message: 'Cadastro enviado com sucesso!',
+        type: 'success',
+      });
+      isRegisterVisible.value = false;
+    })
+    .catch(error => {
+      console.error(error);
+      ElMessage({
+        showClose: true,
+        message: 'Erro ao enviar cadastro!',
+        type: 'error',
+      });
+      isRegisterVisible.value = false;
+    });
 };
 </script>
 
@@ -95,10 +189,13 @@ const callback = async (response) => {
   padding-left: 50px;
   padding-right: 50px;
 }
-.loginDialog {
+.dialog {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.mt-20 {
+  margin-top: 20px;
 }
 img {
   max-height: 25vw;
---
+++ src/stores/store.js
@@ -0,0 +1,30 @@
+import { createPinia } from 'pinia';
+import { defineStore } from 'pinia';
+
+export const pinia = createPinia();
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: JSON.parse(localStorage.getItem('user')),
+    isAuthenticated: localStorage.getItem('isAuthenticated') === 'true',
+    token: null,
+  }),
+  actions: {
+    setUser(user) {
+      this.user = user;
+      this.email = user.email;
+      this.token = user.authentication_token;
+      this.isAuthenticated = true;
+      localStorage.setItem('user', JSON.stringify(user));
+      localStorage.setItem('isAuthenticated', 'true');
+    },
+    logout() {
+      this.user = null;
+      this.email = null;
+      this.token = null;
+      this.isAuthenticated = false;
+      localStorage.removeItem('user');
+      localStorage.removeItem('isAuthenticated');
+    }
+  }
+});
